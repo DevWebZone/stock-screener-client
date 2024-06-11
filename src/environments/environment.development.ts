00c7385637ce7c2c14a203883cfcf2e68364d326
DevWebZone <Devanshu.Wadhwani@gmail.com>
2024-06-11 22:21:03 +0530

    removed creds

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,9 +1,9 @@
 export const environment = {
     production: false,
-    apiClientId: '99d80ae3-3d45-4d18-86b3-d3420f27a663',
-    apiRedirectUrl: 'http://localhost:8080/upstox-redirect',
+    apiClientId: 'YOUR_API_CLIENT_ID',
+    apiRedirectUrl: 'YOUR_REDIRECT_URL',
     authorizationUrl: 'https://api.upstox.com/v2/login/authorization/token',
-    apiClientSecret: '0qg6j4rvq7',
+    apiClientSecret: 'YOUR_API_CLIENT_SECRET',
     marketDataUrl: 'https://api.upstox.com/v2/market-quote/ohlc',
     marketDataFeedUrl: 'https://api.upstox.com/v2/feed/market-data-feed/authorize'
 };
